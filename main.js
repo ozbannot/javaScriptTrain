9651aadab5bb4f5e7b13f24c5bc1559fce716d04
ozbannot <ozbannot@gmail.com>
2021-11-22 21:31:31 +0900

    function commit

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('test');
+// 関数
+const comment = 'テスト';
+
+function getComment(string)
+{
+  console.log(string);
+}
+
+getComment(comment);
+
+function getNumberOfComment()
+{
+  return 5;
+}
+
+console.log(getNumberOfComment());
